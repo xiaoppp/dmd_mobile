a3148ed83c03f18300b080351fd0be8301548f7b
xiaop <xiaop@MacBook.local>
2016-07-10 20:24:06 +0800

    update

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 angular.module('starter.config',['ionic'])
 .constant('config',{
-    host : 'http://192.168.1.101:3000/api/',
-    //host : 'http://112.124.15.7/api/',
+    //host : 'http://192.168.1.101:3000/api/',
+    host : 'http://112.124.15.7/api/',
     loginkey : 'memberid'
 })
 .config(function($ionicConfigProvider) {
